fdcfa0f84d556f9276ecb81057f69daa314e90c1
tiesnoordhuis <tiesnoordhuis@gmail.com>
2020-01-18 16:02:13 +0100

    new version

--- src/Main.elm
+++ src/Main.elm
@@ -8,6 +8,7 @@ module Main exposing (..)
 
 
 import Browser
+import Dropdown
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
@@ -24,17 +25,53 @@ main =
 
 -- MODEL
 
+type Temperature
+    = Celius Float
+    | Farenheid Float
+    | Kelvin Float
+
+toCelcius : Temperature -> Float
+toCelcius temperature =
+    case temperature of
+        Celius tempInCelcius ->
+            tempInCelcius
+        Farenheid tempInFarenheid ->
+            (tempInFarenheid - 32) / 1.8
+        Kelvin tempInKelvin ->
+            tempInKelvin + 273.15
+
+toFarenheid : Temperature -> Float
+toFarenheid temperature =
+    case temperature of
+        Celius tempInCelcius ->
+            tempInCelcius * 1.8 + 32
+        Farenheid tempInFarenheid ->
+            tempInFarenheid
+        Kelvin tempInKelvin ->
+            (tempInKelvin + 273.15) * 1.8 + 32
+
+toKelvin : Temperature -> Float
+toKelvin temperature =
+    case temperature of
+        Celius tempInCelcius ->
+            tempInCelcius - 273.15
+        Farenheid tempInFarenheid ->
+            ((tempInFarenheid - 32) / 1.8) - 273.15
+        Kelvin tempInKelvin ->
+            tempInKelvin
 
 type alias Model = 
     { username : String
     , password : String
     , passwordAgain : String
+    , temperature : Temperature
+    , validTemperature: Bool
     }
 
 
 init : Model
 init =
-    { username = "", password = "", passwordAgain = "" }
+    { username = "", password = "", passwordAgain = "" , temperature = Celius 25, validTemperature = True }
 
 
 -- UPDATE
@@ -44,6 +81,8 @@ type Msg
     = Username String
     | Password String
     | PasswordAgain String
+    | TemperatureUpdate String
+    
 
 
 update : Msg -> Model -> Model
@@ -58,6 +97,14 @@ update msg model =
         PasswordAgain password ->
             { model | passwordAgain = password }
 
+        TemperatureUpdate temperatureInput ->
+            (case String.toFloat temperatureInput of
+                Just temperature -> 
+                    { model | temperature = Celius temperature, validTemperature = True }
+
+                Nothing ->
+                    { model | temperature = Celius 0, validTemperature = False  })
+
 
 
 -- VIEW
@@ -70,6 +117,10 @@ view model =
         , viewInput "password" "Password" model.password Password
         , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
         , viewValidation model
+        , input [ value (String.fromFloat (toCelcius model.temperature)), onInput TemperatureUpdate ] []
+        , div [] [ text ("Celcius = " ++ String.fromFloat (toCelcius model.temperature))]
+        , div [] [ text ("Farenheid = " ++ String.fromFloat (toFarenheid model.temperature))]
+        , div [] [ text ("Kevin = " ++ String.fromFloat (toKelvin model.temperature))]
         ]
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
